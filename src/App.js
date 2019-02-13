a4db7879f6bcfc8885f862586eeb421727696cc3
Michele Saba <michelemsaba@gmail.com>
2019-02-12 21:18:37 -0600

    added river video

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import myVideo from './BokehVideo.mp4';
 import christmasLights from './ChristmasLights.mp4';
 import coloredBokah from './coloredBokah.mp4';
-import morningLights from './morningLights.mp4';
+import river from './river.mp4';
 
 
 class App extends Component {
@@ -13,7 +13,7 @@ class App extends Component {
           <video src={myVideo} className= "myVideo" type="video/mp4" loop autoPlay muted />
           <video src={christmasLights} className= "myVideo" type="video/mp4" loop autoPlay muted />
           <video src={coloredBokah} className= "myVideo" type="video/mp4" loop autoPlay muted />
-          <video src={morningLights} className= "myVideo" type="video/mp4" loop autoPlay muted />
+          <video src={river} className= "myVideo" type="video/mp4" loop autoPlay muted />
       </div>
     );
   }
