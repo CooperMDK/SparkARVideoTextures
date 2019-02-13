89e0311c84f5024c68629acf981bb75e59a63e07
Michele Saba <michelemsaba@gmail.com>
2019-02-12 19:44:19 -0600

    update to video

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import myVideo from './BokehVideo.mp4'
 
@@ -7,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <video className="video" src={myVideo} type="video/mp4" loop autoPlay />
+          <video src={myVideo} className= "myVideo" type="video/mp4" loop autoPlay muted />
       </div>
     );
   }
