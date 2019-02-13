390900901641699691715131d91b0e6c3dd66d5a
Michele Saba <michelemsaba@gmail.com>
2019-02-12 21:00:05 -0600

    added video textures

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-import myVideo from './BokehVideo.mp4'
+import myVideo from './BokehVideo.mp4';
+import christmasLights from './ChristmasLights.mp4';
+import coloredBokah from './coloredBokah.mp4';
+import morningLights from './morningLights.mp4';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           <video src={myVideo} className= "myVideo" type="video/mp4" loop autoPlay muted />
+          <video src={christmasLights} className= "myVideo" type="video/mp4" loop autoPlay muted />
+          <video src={coloredBokah} className= "myVideo" type="video/mp4" loop autoPlay muted />
+          <video src={morningLights} className= "myVideo" type="video/mp4" loop autoPlay muted />
       </div>
     );
   }
